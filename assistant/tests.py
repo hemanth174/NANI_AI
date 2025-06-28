0267dc724fecb286cfda7003e1cfd373d2a2cb17
hemanth174 <ramasaiahemanth@gmail.com>
2025-06-28 16:15:27 +0530

    Delete assistant directory

--- assistant/tests.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-
-class AssistantTests(TestCase):
-    def test_example(self):
-        self.assertEqual(1 + 1, 2)  # Example test case to ensure the test framework is working.
